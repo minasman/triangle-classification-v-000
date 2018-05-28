bdd3250f9f6c03022dbe6879b66477b1d7f74dc4
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-28 18:42:48 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,6 +9,8 @@ class Triangle
   def kind
     if @l1 <= 0 || @l2 <= 0 || @l3 <= 0
       raise TriangleError
+    elsif @l1 + @l2 < @l3 || @l1 + @l3 < @l2 || @l2 + @l3 < @l1
+      raise TriangleError
     elsif @l1 == @l2 && @l1 == @l3
       :equilateral
     elsif @l1 == @l2 || @l1 == @l3 || @l2 == @l3
