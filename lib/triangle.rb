bab6dba7323d2a760d0b9399b9b28a33ff16cf1c
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-28 18:38:08 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,9 @@ class Triangle
   end
   
   def kind
-    if @l1 == @l2 && @l1 == @l3
+    if @l1 == 0 || @l2 == 0 || @l3 == 0
+      raise TriangleError
+    elsif @l1 == @l2 && @l1 == @l3
       :equilateral
     elsif @l1 == @l2 || @l1 == @l3 || @l2 == @l3
       :isosceles
